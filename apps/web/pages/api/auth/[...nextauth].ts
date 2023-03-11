68f620c33b9a5ddc2c09f93c873eb5521b2bcf79
Nick Papadakis <nickfin2014@gmail.com>
2023-03-11 16:33:02 -0700

    add google provider for nextauth

--- apps/web/pages/api/auth/[...nextauth].ts
+++ apps/web/pages/api/auth/[...nextauth].ts
@@ -1,12 +1,12 @@
 import NextAuth from 'next-auth';
-import DiscordProvider from 'next-auth/providers/discord';
+import GoogleProvider from 'next-auth/providers/google';
 
 export default NextAuth({
   // adapter: 'xata',
   providers: [
-    DiscordProvider({
-      clientId: process.env.DISCORD_CLIENT_ID,
-      clientSecret: process.env.DISCORD_CLIENT_SECRET,
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
     /**
      * ...add more providers here.

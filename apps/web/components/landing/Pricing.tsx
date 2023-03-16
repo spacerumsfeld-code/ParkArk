3d37ecdb91ff0d59952905e1a8783005e8b3bd0b
Nick Papadakis <nickfin2014@gmail.com>
2023-03-16 16:07:37 -0600

    pricing mvp

--- apps/web/components/landing/Pricing.tsx
+++ apps/web/components/landing/Pricing.tsx
@@ -1,36 +1,43 @@
 import { CheckIcon } from '@heroicons/react/20/solid';
+import { Button } from '@park-ark/ui/*';
+import router from 'next/router';
 
+/** @Config - pull from CMS eventually */
 const tiers = [
   {
-    name: 'Personal',
-    id: 'tier-personal',
-    href: '#',
-    priceMonthly: '$39',
-    description:
-      "The perfect plan if you're just getting started with our product.",
+    name: 'Freemium',
+    id: 'tier-freemium',
+    priceMonthly: '$0',
+    description: 'Free for life!',
     features: [
-      '25 products',
-      'Up to 10,000 subscribers',
-      'Audience segmentation',
-      'Advanced analytics',
-      'Email support',
-      'Marketing automations',
+      'Up to 10 trips',
+      'Basic notifications and alerts',
+      'Community packing lists and trip reviews',
     ],
-    featured: true,
+    mostPopular: false,
   },
   {
-    name: 'Team',
-    id: 'tier-team',
-    href: '#',
-    priceMonthly: '$99',
-    description: 'A plan that scales with your rapidly growing business.',
+    name: 'Pro',
+    id: 'tier-pro',
+    priceMonthly: '$4.99',
+    description:
+      'Enhance your outdoor experience for the cost of a Starbucks Mocha.',
     features: [
-      'Priority support',
-      'Single sign-on',
-      'Enterprise integrations',
-      'Custom reporting tools',
+      'Unlimited trips',
+      'Advanced AI outdoor chatbot functionality',
+      'Invite your friends to join you on your trip',
+      'Advanced alerts and notifications',
     ],
-    featured: false,
+    mostPopular: true,
+  },
+  {
+    name: 'Lifetime',
+    id: 'tier-lifetime',
+    priceMonthly: '$49',
+    description:
+      'The same features of the Pro plan for life (no monthly fees!)',
+    features: ['see Pro plan'],
+    mostPopular: false,
   },
 ];
 
@@ -38,108 +45,98 @@ function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(' ');
 }
 
-export const LandingPricingSection = () => {
+type LandingPricingSectionProps = {
+  //
+};
+
+export const LandingPricingSection: React.FC<
+  LandingPricingSectionProps
+> = () => {
+  /** @Actions */
+  const handleCTAButtonClick = () => {
+    router.push('/login');
+  };
+
+  /** @Render */
   return (
-    <div
-      id="LandingPricing"
-      className="relative isolate bg-white py-24 px-6 sm:py-32 lg:px-8"
-    >
-      <div
-        className="absolute inset-x-0 -top-3 -z-10 transform-gpu overflow-hidden px-36 blur-3xl"
-        aria-hidden="true"
-      >
-        <svg className="mx-auto w-[72.1875rem]" viewBox="0 0 1155 678">
-          <path
-            fill="url(#083c110e-60c8-42bb-a9db-29bba74cc569)"
-            fillOpacity=".3"
-            d="M317.219 518.975L203.852 678 0 438.341l317.219 80.634 204.172-286.402c1.307 132.337 45.083 346.658 209.733 145.248C936.936 126.058 882.053-94.234 1031.02 41.331c119.18 108.451 130.68 295.337 121.53 375.223L855 299l21.173 362.054-558.954-142.079z"
-          />
-          <defs>
-            <linearGradient
-              id="083c110e-60c8-42bb-a9db-29bba74cc569"
-              x1="1155.49"
-              x2="-78.208"
-              y1=".177"
-              y2="474.645"
-              gradientUnits="userSpaceOnUse"
-            >
-              <stop stopColor="#9089FC" />
-              <stop offset={1} stopColor="#FF80B5" />
-            </linearGradient>
-          </defs>
-        </svg>
-      </div>
-      <div className="mx-auto max-w-2xl text-center lg:max-w-4xl">
-        <h2 className="text-base font-semibold leading-7 text-indigo-600">
-          Pricing
-        </h2>
-        <p className="mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">
-          The right price for you, whoever you are
-        </p>
-      </div>
-      <p className="mx-auto mt-6 max-w-2xl text-center text-lg leading-8 text-gray-600">
-        Qui iusto aut est earum eos quae. Eligendi est at nam aliquid ad quo
-        reprehenderit in aliquid fugiat dolorum voluptatibus.
-      </p>
-      <div className="mx-auto mt-16 grid max-w-lg grid-cols-1 items-center gap-y-6 sm:mt-20 sm:gap-y-0 lg:max-w-4xl lg:grid-cols-2">
-        {tiers.map((tier, tierIdx) => (
-          <div
-            key={tier.id}
-            className={classNames(
-              tier.featured
-                ? 'relative bg-white shadow-2xl'
-                : 'bg-white/60 sm:mx-8 lg:mx-0',
-              tier.featured
-                ? ''
-                : tierIdx === 0
-                ? 'rounded-t-3xl sm:rounded-b-none lg:rounded-tr-none lg:rounded-bl-3xl'
-                : 'sm:rounded-t-none lg:rounded-tr-3xl lg:rounded-bl-none',
-              'rounded-3xl p-8 ring-1 ring-gray-900/10 sm:p-10'
-            )}
-          >
-            <h3
-              id={tier.id}
-              className="text-base font-semibold leading-7 text-indigo-600"
-            >
-              {tier.name}
-            </h3>
-            <p className="mt-4 flex items-baseline gap-x-2">
-              <span className="text-5xl font-bold tracking-tight text-gray-900">
-                {tier.priceMonthly}
-              </span>
-              <span className="text-base text-gray-500">/month</span>
-            </p>
-            <p className="mt-6 text-base leading-7 text-gray-600">
-              {tier.description}
-            </p>
-            <ul
-              role="list"
-              className="mt-8 space-y-3 text-sm leading-6 text-gray-600 sm:mt-10"
-            >
-              {tier.features.map((feature) => (
-                <li key={feature} className="flex gap-x-3">
-                  <CheckIcon
-                    className="h-6 w-5 flex-none text-indigo-600"
-                    aria-hidden="true"
-                  />
-                  {feature}
-                </li>
-              ))}
-            </ul>
-            <a
-              href={tier.href}
-              aria-describedby={tier.id}
+    <div id="LandingPricingSection" className="bg-white py-24 sm:py-32">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto max-w-4xl text-center">
+          <h2 className="text-base font-semibold leading-7 text-grey-600">
+            Pricing
+          </h2>
+          <p className="mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">
+            Do your part - Ten percent of all profits go right back to national
+            parks
+          </p>
+        </div>
+        {/* <p className="mx-auto mt-6 max-w-2xl text-center text-lg leading-8 text-gray-600">
+          For the price of a cup of coffee, you can help support the national
+          parks.
+        </p> */}
+        <div className="isolate mx-auto mt-16 grid max-w-md grid-cols-1 gap-y-8 sm:mt-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+          {tiers.map((tier, index) => (
+            <div
+              key={tier.id}
               className={classNames(
-                tier.featured
-                  ? 'bg-indigo-600 text-white shadow hover:bg-indigo-500'
-                  : 'text-indigo-600 ring-1 ring-inset ring-indigo-200 hover:ring-indigo-300',
-                'mt-8 block rounded-md py-2.5 px-3.5 text-center text-sm font-semibold focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 sm:mt-10'
+                tier.mostPopular ? 'lg:z-10 lg:rounded-b-none' : 'lg:mt-8',
+                index === 0 ? 'lg:rounded-r-none' : '',
+                index === tiers.length - 1 ? 'lg:rounded-l-none' : '',
+                'flex flex-col justify-between rounded-3xl bg-white p-8 ring-1 ring-gray-200 xl:p-10'
               )}
             >
-              Get started today
-            </a>
-          </div>
-        ))}
+              <div>
+                <div className="flex items-center justify-between gap-x-4">
+                  <h3
+                    id={tier.id}
+                    className={classNames(
+                      tier.mostPopular ? 'text-green-600' : 'text-gray-900',
+                      'text-lg font-semibold leading-8'
+                    )}
+                  >
+                    {tier.name}
+                  </h3>
+                  {tier.mostPopular ? (
+                    <p className="rounded-full bg-green-600/10 py-1 px-2.5 text-xs font-semibold leading-5 text-green-600">
+                      Most popular
+                    </p>
+                  ) : null}
+                </div>
+                <p className="mt-4 text-sm leading-6 text-gray-600">
+                  {tier.description}
+                </p>
+                <p className="mt-6 flex items-baseline gap-x-1">
+                  <span className="text-4xl font-bold tracking-tight text-gray-900">
+                    {tier.priceMonthly}
+                  </span>
+                  <span className="text-sm font-semibold leading-6 text-gray-600">
+                    /month
+                  </span>
+                </p>
+                <ul
+                  role="list"
+                  className="mt-8 space-y-3 text-sm leading-6 text-gray-600"
+                >
+                  {tier.features.map((feature) => (
+                    <li key={feature} className="flex gap-x-3">
+                      <CheckIcon
+                        className="h-6 w-5 flex-none text-green-600"
+                        aria-hidden="true"
+                      />
+                      {feature}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+              <Button
+                className="bg-green-600 text-white shadow-sm hover:bg-green-500"
+                onClick={() => handleCTAButtonClick()}
+              >
+                <Button.Label>Buy plan</Button.Label>
+              </Button>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
